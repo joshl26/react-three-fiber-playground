32499bb6d8781175ece4ec92e10dd95ed047931a
Joshua Lehman <joshlehman.dev@gmail.com>
2023-01-09 00:13:28 -0500

    cpmmot

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import "./App.module.scss";
+import Scene from "./components/Scene";
+import Scene1 from "./components/Scene1";
 
 function App() {
   return (
     <div className="App">
       <h1>Hello World!!</h1>
+      <Scene />
+      <Scene1 />
     </div>
   );
 }
